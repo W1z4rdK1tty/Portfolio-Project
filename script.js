fff0befbfd97c02f9314e544866feae3886f3fc2
Sophie Preston <sophkitty1986+gh@gmail.com>
2023-10-19 15:09:10 +0100

    Added a modal box and styled for contact me section - instead of previously done mailto

--- script.js
+++ script.js
@@ -91,17 +91,25 @@ scrollTop.addEventListener("click", function(){
     })
 })
 
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
 
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  };
+  
+  openModalBtn.addEventListener("click", openModal);
 
+  const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  };
+  
+  closeModalBtn.addEventListener("click", closeModal);
 
+  overlay.addEventListener("click", closeModal);
 
-
-const contactBtn = document.getElementById('contactMe')
-
-contactBtn.addEventListener('click', emailPopUp)
-
-function emailPopUp() {
-    // window.alert("Email is my preferred method of communication")
-    window.open("mailto:dummyemail@gmail.com")
-}
 
